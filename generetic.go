a2de316387622bc691b7ab61dd831ec07fe94012
Seraphine_Tsui <chi811008@gmail.com>
2024-06-01 20:24:56 +0800

    feat: 泛形

---
+++ generetic.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+    var s V
+    for _, v := range m {
+        s += v
+    }
+    return s
+}
+
+type Number interface {
+    int64 | float64
+}
+
+// SumNumbers sums the values of map m. It supports both integers
+// and floats as map values.
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+    var s V
+    for _, v := range m {
+        s += v
+    }
+    return s
+}
+
+
+type Complex interface {
+	~complex64 | ~complex128
+}
+
+type Float interface {
+    ~float32 | ~float64
+}
+
+type Unsigned interface {
+    ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+func example () {
+    // Initialize a map for the integer values
+	ints := map[string]int64{
+		"first":  34,
+		"second": 12,
+	}
+	
+	// Initialize a map for the float values
+	floats := map[string]float64{
+		"first":  35.98,
+		"second": 26.99,
+	}
+	
+	fmt.Printf("Generic Sums: %v and %v\n",
+    	SumIntsOrFloats[string, int64](ints),
+    	SumIntsOrFloats[string, float64](floats))
+
+	fmt.Printf("Interface Sums: %v and %v\n",
+		SumNumbers[string, int64](ints),
+		SumNumbers[string, float64](floats))
+}
