9009b4260a8e5f3c062305ddc8402691d7e05815
Seraphine_Tsui <chi811008@gmail.com>
2024-06-02 11:27:47 +0800

    feat: iter.Pull2

--- range_iter.go
+++ range_iter.go
@@ -7,12 +7,15 @@ import (
 
 func main() {
 
-	ns := []int{10, 20, 30, 40}
+	ns := []int{10, 20, 30, 40, 50, 60}
 	for v1, v2 := range Pair(All(ns)) {
 		fmt.Println(v1, v2)
 	}
 
-
+	m := map[string]int{"a": 1, "b": 2, "c": 3}
+	for k, v := range KVPair(All2(m)) {
+		fmt.Println(k, v)
+	}
 }
 
 // type Seq0 func(yield func() bool) bool
@@ -36,6 +39,28 @@ func Pair[V any](seq iter.Seq[V]) iter.Seq2[V, V] {
 		for {
 			v1, _ := next(); v2, ok := next()
 			if !ok || !yield(v1, v2) { break }
+		}
+	}
+}
+
+func All2[K comparable, V any](m map[K]V) iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for k, v := range m {
+			if !yield(k, v) {
+				break
+			}
+		}
+	}
+}
+// iter.Pull2 
+// func Pull2[K, V any](seq Seq2[K, V]) (next func() (K, V, bool), stop func())
+func KVPair[K, V any](seq iter.Seq2[K, V]) iter.Seq2[K, V] {
+	next, stop := iter.Pull2(seq)
+	return func(yield func(K, V) bool) {
+		defer stop()
+		for {
+			k, v, ok := next()
+			if !ok || !yield(k, v) { break }
 		}
 	}
 }
